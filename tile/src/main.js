aaed69e1a3abe18482fc948579f4c05d51b82590
mabelsueh <su.eh.mabel@gmail.com>
2020-12-19 10:28:59 +0000

    added vue-router

--- tile/src/main.js
+++ tile/src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import Scoreboard from './components/Scoreboard'
 
 Vue.config.productionTip = false
 
+const routes = [
+    {path:'/scoreboard', component: Scoreboard}
+]
+
+const router = new VueRouter({
+    routes
+})
+
+Vue.use(VueRouter)
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
