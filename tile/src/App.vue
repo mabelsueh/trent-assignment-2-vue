91402b266a3a0354ce2b202bcdabbad06b2e7485
mabelsueh <su.eh.mabel@gmail.com>
2020-12-19 10:29:42 +0000

    set up app.vue & scoreboard.vue

--- tile/src/App.vue
+++ tile/src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <h4><router-link to="/scoreboard">Leaderboard</router-link></h4>
+    <router-view></router-view>
+    <Scoreboard />
+  </div>
 </template>
 
 <script>
-export default {};
+import Scoreboard from "./components/Scoreboard.vue";
+export default {
+  name: "App",
+  components: { Scoreboard },
+};
 </script>
 
 <style scoped>
---
+++ tile/src/components/Scoreboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Leaderboard</h1>
+    <button v-on:ch5ck="loadScoreboard">View</button>
+    <div v-for="s in scoreboard" v-bind:key="s._id">
+        <h5>Username: {{ s.username }}</h5>
+        <h5>Time Taken: {{ s.scoreinSeconds }}</h5>
+        <h5>Date Played: {{ s.datePlayed }}</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      'scoreboard': [],
+    };
+  },
+
+  methods: {
+    loadScoreboard: async function () {
+      let response = await axios.get(
+        "https://3000-e476020e-f0fb-4276-a53d-413c0ee348dd.ws-us03.gitpod.io/scoreboard"
+      );
+      this.scoreboard = response.data;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
