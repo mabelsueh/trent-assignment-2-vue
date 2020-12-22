741524b15524bb92e26ee490afbacfb5a2ef4f71
mabelsueh <su.eh.mabel@gmail.com>
2020-12-22 11:36:02 +0000

    created sample login form

---
+++ tile/src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div>
+            <label>Email: </label>
+        <input type="email" />
+        </div>
+        <div>
+            <label>Password: </label>
+            <input type="text" />
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'Login',
+    components: {}
+    
+}
+</script>
+
+<style scoped>
+
+</style>
--- tile/src/main.js
+++ tile/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 
 import Scoreboard from './components/Scoreboard'
+import Login from './components/Login'
 
 Vue.config.productionTip = false
 
 const routes = [
-    {path:'/scoreboard', component: Scoreboard}
+    {path:'/scoreboard', component: Scoreboard},
+    {path:'/login', component: Login}
 ]
 
 const router = new VueRouter({
