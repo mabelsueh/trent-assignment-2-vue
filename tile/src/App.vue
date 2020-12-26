dca74c3c82220145eb005252f4219a425c2a07b7
mabelsueh <su.eh.mabel@gmail.com>
2020-12-26 09:22:29 +0000

    added routes

--- tile/src/App.vue
+++ tile/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
+    <Alerts message="I WILL BE MOVED TO LOGIN.VUE!"/>
+    <h3><router-link to="/game">Game</router-link></h3>
+    <h4><router-link to="/play">Play</router-link></h4>
     <h4><router-link to="/scoreboard">Leaderboard</router-link></h4>
+    <h4><router-link to="/login">Login</router-link></h4>
+    <h4><router-link to="/register">Register</router-link></h4>
     <router-view></router-view>
-    <Scoreboard />
   </div>
 </template>
 
 <script>
-import Scoreboard from "./components/Scoreboard.vue";
+import Alerts from './components/Alerts.vue'
+
 export default {
   name: "App",
-  components: { Scoreboard },
+  components: {Alerts },
 };
 </script>
 
--- tile/src/main.js
+++ tile/src/main.js
@@ -2,14 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 
+import Game from './components/Game'
+import TileGame from './components/TileGame'
 import Scoreboard from './components/Scoreboard'
 import Login from './components/Login'
 
 Vue.config.productionTip = false
 
 const routes = [
+    {path:'/game', component: Game},
+    {path:'/play', component: TileGame},
     {path:'/scoreboard', component: Scoreboard},
-    {path:'/login', component: Login}
+    {path:'/login', component: Login},
+    // {path:'/register', component: Register}
 ]
 
 const router = new VueRouter({
