e16fce59958c674d1360eecc16cee95a7f6cf494
mabelsueh <su.eh.mabel@gmail.com>
2020-12-26 09:20:56 +0000

    created alert

---
+++ tile/src/components/Alerts.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="alerts">
+        {{message}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Alerts",
+    props:['message']
+    
+}
+</script>
+
+<style scoped>
+.alerts {
+    background-color: pink;
+}
+
+</style>
