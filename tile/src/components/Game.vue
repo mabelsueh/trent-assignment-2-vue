37a73d81cbd180c8270612e5da220834953cec81
mabelsueh <su.eh.mabel@gmail.com>
2020-12-30 08:02:26 +0000

    centered game.vue

--- tile/src/components/Game.vue
+++ tile/src/components/Game.vue
@@ -44,46 +44,57 @@ export default {
   methods: {
     //   change: function () {
     //   this.$set(this.board[i], j, Math.floor(Math.random() * 10));
-    },
-    //     shuffle: function () {
-    //         let colors = ["red", "yellow", "blue", "green"];
-    //   let tiles = [];
-    //   for (let c of colors) {
-    //     for (let i = 0; i <= 7; i++) {
-    //       // for each number from 1 to 9
-    //       for (let j = 0; j < 2; j++) {
-    //         // put that tile onto the deck
-    //         tiles.push({
-    //           value: i,
-    //           color: c,
-    //         });
-    //       }
-    //     }
-    //   }
-    //   for (let s = 0; s < 20000; s++) {
-    //     let firstIndex = Math.floor(Math.random() * tiles.length);
-    //     let secondIndex = Math.floor(Math.random() * tiles.length);
-    //     let temp = tiles[firstIndex];
-    //     tiles[firstIndex] = tiles[secondIndex];
-    //     tiles[secondIndex] = temp;
-    //   }
-    //   for (let tile of tiles) {
-    //     this.deck.push(tile);
-    //   }
-    // },
+  },
+  //     shuffle: function () {
+  //         let colors = ["red", "yellow", "blue", "green"];
+  //   let tiles = [];
+  //   for (let c of colors) {
+  //     for (let i = 0; i <= 7; i++) {
+  //       // for each number from 1 to 9
+  //       for (let j = 0; j < 2; j++) {
+  //         // put that tile onto the deck
+  //         tiles.push({
+  //           value: i,
+  //           color: c,
+  //         });
+  //       }
+  //     }
+  //   }
+  //   for (let s = 0; s < 20000; s++) {
+  //     let firstIndex = Math.floor(Math.random() * tiles.length);
+  //     let secondIndex = Math.floor(Math.random() * tiles.length);
+  //     let temp = tiles[firstIndex];
+  //     tiles[firstIndex] = tiles[secondIndex];
+  //     tiles[secondIndex] = temp;
+  //   }
+  //   for (let tile of tiles) {
+  //     this.deck.push(tile);
+  //   }
+  // },
 };
 </script>
 
 <style scoped>
-/* #Game {
-    text-align: center;
+div {
+    display: flex;
+    flex-direction:column;
     justify-content: center;
-} */
+}
+
+h1 {
+    text-align: center;   
+}
+
+table {
+  margin: auto;
+
+}
 
 td {
-  height: 80px;
-  width: 80px;
+  height: 60px;
+  width: 60px;
   text-align: center;
   border: 1px solid gray;
+
 }
 </style>
