7ac2a2b649d6011875dfce89e3cfca7d44994a66
mabelsueh <su.eh.mabel@gmail.com>
2021-01-10 10:13:24 +0000

    added scoreboard table

--- tile/src/components/Scoreboard.vue
+++ tile/src/components/Scoreboard.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
     <h1>Leaderboard</h1>
-    <button v-on:click="loadScoreboard">View</button>
-    <div v-for="s in scoreboard" v-bind:key="s._id">
-        <!-- use bootstrap-vue or something -->
-        <h5>Username: {{ s.username }}</h5>
-        <h5>Time Taken: {{ s.scoreInSeconds }}</h5>
-        <h5>Date Played: {{ s.datePlayed }}</h5>
-    </div>
+    <b-table-simple class="table">
+        <b-thead class="header">
+
+            <b-tr>
+                <b-th>Username</b-th>
+                <b-th>Score</b-th>
+                <b-th>Date Played</b-th>
+            </b-tr>
+        </b-thead>
+        <b-tbody>
+            <b-tr v-for="s in scoreboard.slice(0,10)" v-bind:key="s._id">
+        <b-td>{{ s.username }}</b-td>
+        <b-td>{{ s.score }}</b-td>
+        <b-td>{{ s.datePlayed }}</b-td>
+            </b-tr>
+        </b-tbody>
+    </b-table-simple>
+
+
   </div>
 </template>
 
@@ -19,15 +31,21 @@ export default {
       'scoreboard': [],
     };
   },
-
-  methods: {
-    loadScoreboard: async function () {
+  created: async function () {
       let response = await axios.get(
         "https://3000-e476020e-f0fb-4276-a53d-413c0ee348dd.ws-us03.gitpod.io/scoreboard"
       );
       this.scoreboard = response.data;
     },
-  },
+
+//   methods: {
+//     loadScoreboard: async function () {
+//       let response = await axios.get(
+//         "https://3000-e476020e-f0fb-4276-a53d-413c0ee348dd.ws-us03.gitpod.io/scoreboard"
+//       );
+//       this.scoreboard = response.data;
+//     },
+//   },
 };
 </script>
 
