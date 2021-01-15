7536db102200b8c247cfd3148836b5520af7098d
mabelsueh <su.eh.mabel@gmail.com>
2021-01-15 10:35:32 +0000

    nav-logo directory update

--- tile/vue.config.js
+++ tile/vue.config.js
@@ -2,5 +2,7 @@ module.exports = {
     // options...
     devServer: {
         disableHostCheck: true
-    }
+    },
+    publicPath: process.env.NODE_ENV === "production" ? "/trent-assignment-2-vue/" : "/"
+
 }
