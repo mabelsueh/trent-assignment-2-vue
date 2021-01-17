7b4f3d7395e6698fbe4353f0b9a4683eb9f95d63
mabelsueh <su.eh.mabel@gmail.com>
2021-01-17 10:43:24 +0000

    updated navs, links are working

--- tile/src/components/Nav.vue
+++ tile/src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div id="logo" align="center" class="mb-0">
-      <b-nav-item to="/" style="list-style-type: none">
+      <b-nav-item to="/play" style="list-style-type: none">
         <img src="images/nav-logo.png" width="15%" alt="navlogo" />
       </b-nav-item>
     </div>
@@ -9,9 +9,9 @@
     <div id="navlinks" class="mt-0">
       <b-nav align="center">
         <b-nav-item class="mx-4" to="/scoreboard">Leaderboard</b-nav-item>
-        <b-nav-item class="mx-4" to="/play">Play</b-nav-item>
-        <b-nav-item class="mx-4" to="/login">Login</b-nav-item>
-        <b-nav-item class="mx-4" to="/register">Register</b-nav-item>
+        <!-- <b-nav-item class="mx-4" to="/play">Start Playing</b-nav-item> -->
+        <!-- <b-nav-item class="mx-4" to="/login">Login</b-nav-item>
+        <b-nav-item class="mx-4" to="/register">Register</b-nav-item> -->
       </b-nav>
     </div>
   </div>
@@ -31,7 +31,7 @@ export default {};
 a {
   color: #edcd33 !important;
   font-family: "Creepster", cursive;
-  letter-spacing: 0.2em;
+  letter-spacing: 0.3em;
 }
 
 a:hover {
